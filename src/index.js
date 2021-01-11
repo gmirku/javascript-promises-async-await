a10254bc4a365e805340a539aa5ebaee5b76e7ee
gmirku <76975353+gmirku@users.noreply.github.com>
2021-01-11 13:12:48 +0100

    module3

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import { fetchWithTimeout } from './services.js'
+import { fetchWithTimeout } from './services';
 
 const movies = require('./data/movies.json');
 
 export function fetchMovies(){
-  const resolveFunction=()=>movies;
-  return fetchWithTimeout(1000)
-  .then(resolveFunction);
+  const resolveFunction= () => movies;
+  return fetchWithTimeout(1000).then(resolveFunction);
 }
 
-let moviePromise=fetchMovies;
-moviePromise
-.then(resolve(results){
+const moviePromise=fetchMovies();
+moviePromise.then( results => {
   console.log(results);
-}
+});
