ca7e7ac2a25407c20da5105f13992958f962991a
gmirku <76975353+gmirku@users.noreply.github.com>
2021-01-11 13:22:58 +0100

    module4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import { fetchWithTimeout } from './services';
+import { fetchMovies,fetchBooks } from './services';
 
 const movies = require('./data/movies.json');
 
-export function fetchMovies(){
-  const resolveFunction= () => movies;
-  return fetchWithTimeout(1000).then(resolveFunction);
+function getBooksAndMovies(){
+  return Promise.all([fetchBooks(),fetchMovies()])
+  .then(([books, movies ]) => ({
+  books,
+  movies
+}))
+.catch(error => console.log("Error fetching books and movies", error));
+}
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise
+.then(results => console.log('getBooksAndMoviesPromise', results));
+
+function getBooksOrMovies(){
+  Promise.race([fetchBooks(),fetchMovies()])
+  .then(results => results)
+.catch(error => console.log("Error waiting for the promise race", error));
 }
 
-const moviePromise=fetchMovies();
-moviePromise.then( results => {
-  console.log(results);
-});
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then(results=>)console.log('getBooksOrMoviesPromise', results);
